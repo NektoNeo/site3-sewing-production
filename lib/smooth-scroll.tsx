791a2848d6a24582b986eb2f7fa909a1fdd0bd27
NektoNeo <thenekto_neo@mail.ru>
2025-10-27 12:57:20 +0400

    revert(smooth-scroll): откат GSAP интеграции для восстановления текста
    Откатываем изменения в lib/smooth-scroll.tsx которые вызвали
    исчезновение текста на большинстве разделов сайта.
    
    Проблема была связана с асинхронной инициализацией GSAP ScrollTrigger,
    которая нарушала рендеринг контента.
    
    Возвращаемся к стабильной версии Lenis smooth scroll без GSAP.

--- lib/smooth-scroll.tsx
+++ lib/smooth-scroll.tsx
@@ -14,50 +14,25 @@ export function SmoothScroll({ children }: { children: React.ReactNode }) {
       return;
     }
 
-    let ScrollTrigger: any;
-
-    const init = async () => {
-      // Динамический импорт GSAP ScrollTrigger
-      const gsap = (await import("gsap")).default;
-      const { ScrollTrigger: ST } = await import("gsap/ScrollTrigger");
-      ScrollTrigger = ST;
-      gsap.registerPlugin(ScrollTrigger);
-
-      const lenis = new Lenis({
-        duration: 1.2,
-        easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-        orientation: "vertical",
-        gestureOrientation: "vertical",
-        smoothWheel: true,
-        wheelMultiplier: 1,
-        touchMultiplier: 2,
-      });
-
-      // КРИТИЧНО: Синхронизация Lenis с GSAP ScrollTrigger
-      lenis.on("scroll", ScrollTrigger.update);
-
-      // Обновляем ScrollTrigger при изменении размера
-      gsap.ticker.add((time) => {
-        lenis.raf(time * 1000);
-      });
-
-      gsap.ticker.lagSmoothing(0);
-
-      // Cleanup
-      return () => {
-        lenis.destroy();
-        ScrollTrigger?.getAll().forEach((t: any) => t.kill());
-        gsap.ticker.remove(() => {});
-      };
-    };
-
-    let cleanup: (() => void) | undefined;
-    init().then((cleanupFn) => {
-      cleanup = cleanupFn;
+    const lenis = new Lenis({
+      duration: 1.2,
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+      orientation: "vertical",
+      gestureOrientation: "vertical",
+      smoothWheel: true,
+      wheelMultiplier: 1,
+      touchMultiplier: 2,
     });
 
+    function raf(time: number) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+
     return () => {
-      cleanup?.();
+      lenis.destroy();
     };
   }, []);
 
